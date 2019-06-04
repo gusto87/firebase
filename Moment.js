d6b41ca323f49770d07937d4e26ac1997aafdb95
will <wsherrill17@gmail.com>
2019-06-03 20:11:42 -0400

    addedappendinginfo

--- Moment.js
+++ Moment.js
@@ -12,27 +12,31 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-  var dataRef = firebase.database();
+  var database = firebase.database();
 // values for table 
   var trainName = "";
   var destination = "";
-  var time = 0;
-  var frequency = "";
+  var time = "";
+  var frequency = 0;
 
 // action for button click
 $("#add-info").on("click", function(event){
   event.preventDefault();
 
   // Values from text boxes 
-  trainName = $("#train-name-input").val().trim();
-  destination = $("#destination-input").val().trim();
-   time = $("#time-input").val().trim();
-   frequency = $("#frequency-input").val().trim();
+ var trainName = $("#train-name-input").val().trim();
+ var destination = $("#destination-input").val().trim();
+ var time = $("#time-input").val().trim();
+ var frequency = $("#frequency-input").val().trim();
 
+   console.log(trainName);
+   console.log(destination);
+   console.log(time);
+   console.log(frequency);
    // Setting values in the database
 
    var newTrain = {
-     trainName: trainName,
+     traiName: trainName,
      destination: destination,
      time: time,
      frequency: frequency,
@@ -41,34 +45,57 @@ $("#add-info").on("click", function(event){
 
 
 // watcher for firebase and inital loader
-database.ref().push(newTrain)
+database.ref().push({
+newTrain
+});
 
-// alert new train info
-alert("Train successfully added");
-  
+return false;
 
-// Clears the input of the text box
-$("#train-name-input").val("");
-$("#destination-input").val("");
-$("#time-input").val("");
-$("#frequency-input").val("");
   });
 
   // Firebase event adding trains to database and row in html for new entry
-  database.ref().on("added_child", function(childSnapshot, prevChildKey){
-    console.log(childSnapshot.val());
+  database.ref().on("child_added", function(snaphot){
+    console.log(snapshot.val());
 
-    var trainName = childSnapshot.val().name;
-    var destination = childSnapshot.val().destination
-    var time = childSnapshot.val().time;
-    var frequency = childSnapshot.val().frequency;
+    // stored into variables
+    var trainName = snapshot.val().trainName;
+    var destination = snapshot.val().destination
+    var time = snapshot.val().time;
+    var frequency = snapshot.val().frequency;
 
-    // declare varible
-    var frequency;
 
-    // time on the entry form
-    var time = 0;
+    var intialArrival = moment(time, "HH:mm")
+    // creates object of the present date and time and storing it in varible until called by submit button
+    var newMoment = moment();
+    // does math of trains arrival in minutes
+    var elaspedMinSinceArr = newMoment.diff(intialArrival, 'minutes');
+    var lastArrivalMin = elaspedMinSinceArr % frequency;
+    var minUntilArr = frequency - lastArrivalMin;
 
-    var 
+    var nextTrain = newMoment.add(minUntilArr, 'minutes');
+     formatNextTrain = nextTrain.format("HH:mm");
 
-  })
+    // creates new info for table
+    var tr = $('<tr>');
+    var a = $('<td>');
+    var b = $('<td>');
+    var c = $('<td>');
+    var d = $('<td>');
+    var e = $('<td>');
+    a.append(trainName);
+    b.append(destination);
+    c.append(frequency);
+    d.append(formatNextTrain);
+    e.append(minUntilArr);
+    tr.append(a).append(b).append(c).append(d).append(e);
+    $('#newTrainInfo').append(tr);
+    // function for the errors
+    }, function (errorObject){
+      console.log("The read failed" + errorObject.code);
+   
+    });
+  
+    
+  
+  
+  
