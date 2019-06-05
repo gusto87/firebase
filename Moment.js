dc45837d7a2da43e457f3d58d716c5efc142f459
will <wsherrill17@gmail.com>
2019-06-04 23:58:43 -0400

    gotfirebaseworking

--- Moment.js
+++ Moment.js
@@ -1,27 +1,28 @@
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyAa3EB3WbRpVfsRDcPU0Y9dSnrondwOwpE",
-    authDomain: "week7-3-f0ac9.firebaseapp.com",
-    databaseURL: "https://week7-3-f0ac9.firebaseio.com",
-    projectId: "week7-3-f0ac9",
-    storageBucket: "week7-3-f0ac9.appspot.com",
-    messagingSenderId: "1076171700414",
-    appId: "1:1076171700414:web:fec2ad0f77d23344"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-
+ 
+  
+  
+  
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyAa3EB3WbRpVfsRDcPU0Y9dSnrondwOwpE",
+      authDomain: "week7-3-f0ac9.firebaseapp.com",
+      databaseURL: "https://week7-3-f0ac9.firebaseio.com",
+      projectId: "week7-3-f0ac9",
+      storageBucket: "week7-3-f0ac9.appspot.com",
+      messagingSenderId: "1076171700414",
+      appId: "1:1076171700414:web:fec2ad0f77d23344"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+   
   var database = firebase.database();
-// values for table 
-  var trainName = "";
-  var destination = "";
-  var time = "";
-  var frequency = 0;
 
+// values for table 
+  
 // action for button click
-$("#add-info").on("click", function(event){
-  event.preventDefault();
+$("#add-info").on("click", function(childSnaphot){
+childSnaphot.preventDefault();
 
   // Values from text boxes 
  var trainName = $("#train-name-input").val().trim();
@@ -29,68 +30,79 @@ $("#add-info").on("click", function(event){
  var time = $("#time-input").val().trim();
  var frequency = $("#frequency-input").val().trim();
 
-   console.log(trainName);
-   console.log(destination);
-   console.log(time);
-   console.log(frequency);
+  
    // Setting values in the database
 
    var newTrain = {
-     traiName: trainName,
+     name: trainName,
      destination: destination,
      time: time,
      frequency: frequency,
     };
    
+    // check values via if/else statements
+    if (
+      trainName != "" &&
+      destination != "" &&
+      frequency != "") {
+        //uploads data to firebase
+        database.ref().push(newTrain);
+        alert ("New train added!")
+      }else{
+        alert("Invalid entry/format")
+      }
+    
+      // stores data in the console
+      console.log(newTrain.trainName)
+      console.log(newTrain.destination)
+      console.log(newTrain.frequency)
+      console.log(newTrain.time)
+     
+      // Clear input boxes
+      $("#train-name-input").val("");
+      $("#destination-input").val("");
+      $("#time-input").val("");
+      $("#frequency-input").val("");
 
-
-// watcher for firebase and inital loader
-database.ref().push({
-newTrain
 });
 
-return false;
-
-  });
 
   // Firebase event adding trains to database and row in html for new entry
-  database.ref().on("child_added", function(snaphot){
-    console.log(snapshot.val());
+  database.ref().on("child_added", function(childSnapshot) {
 
     // stored into variables
-    var trainName = snapshot.val().trainName;
-    var destination = snapshot.val().destination
-    var time = snapshot.val().time;
-    var frequency = snapshot.val().frequency;
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().destination;
+    var time = childSnapshot.val().time;
+    var frequency = childSnapshot.val().frequency;
 
+    console.log(trainName);
+    console.log(destination);
+    console.log(frequency);
+    console.log(time)
+    
+   
+    // does math of trains arrival time
+    var elaspedMinSinceArr = moment(childSnapshot.val().time, "HH:mm").subtract(1, "years");
+    var lastArrivalMin = moment().diff(moment(elaspedMinSinceArr), "minutes");
+    var minUntilArr = lastArrivalMin % childSnapshot.val().frequency;
 
-    var intialArrival = moment(time, "HH:mm")
-    // creates object of the present date and time and storing it in varible until called by submit button
-    var newMoment = moment();
-    // does math of trains arrival in minutes
-    var elaspedMinSinceArr = newMoment.diff(intialArrival, 'minutes');
-    var lastArrivalMin = elaspedMinSinceArr % frequency;
-    var minUntilArr = frequency - lastArrivalMin;
+    var timeUntilArr = childSnapshot.val().frequency - minUntilArr;
 
-    var nextTrain = newMoment.add(minUntilArr, 'minutes');
-     formatNextTrain = nextTrain.format("HH:mm");
-
+    var nextArrivalTemp = moment().add(timeUntilArr, "minutes");
+    nextArrival = moment(nextArrivalTemp).format("HH:mm")
     // creates new info for table
-    var tr = $('<tr>');
-    var a = $('<td>');
-    var b = $('<td>');
-    var c = $('<td>');
-    var d = $('<td>');
-    var e = $('<td>');
-    a.append(trainName);
-    b.append(destination);
-    c.append(frequency);
-    d.append(formatNextTrain);
-    e.append(minUntilArr);
-    tr.append(a).append(b).append(c).append(d).append(e);
-    $('#newTrainInfo').append(tr);
+    var newInfo = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(destination),
+      $("<td>").text(frequency),
+      $("<td>").text(nextArrival),
+      $("<td>").text(timeUntilArr),
+    );
+
+    $("#big-trains").append(newInfo);
     // function for the errors
-    }, function (errorObject){
+  }, function (errorObject){
       console.log("The read failed" + errorObject.code);
    
     });
